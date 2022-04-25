f1ac1842f34728427d17c046400b3ce16b45e120
Ahmad Tariq <75003816+ahmadbintariq4u@users.noreply.github.com>
2022-04-25 14:19:38 +0500

    icon + Splash Screen Updated

--- lib/controllers/home/home_controller.dart
+++ lib/controllers/home/home_controller.dart
@@ -33,52 +33,52 @@ class HomeController extends GetxController
     //   },
     // );
 
-    FirebaseMessaging.instance.getInitialMessage().then(
-      (message) async {
-        print("FirebaseMessaging.instance.getInitialMessage");
-        if (message != null) {
-          Get.dialog(const Text('hello'));
+    // FirebaseMessaging.instance.getInitialMessage().then(
+    //   (message) async {
+    //     print("FirebaseMessaging.instance.getInitialMessage");
+    //     if (message != null) {
+    //       Get.dialog(const Text('hello'));
 
-          print('terminated state');
+    //       print('terminated state');
 
-          if (message.data['update'] != null) {
-            final box = await Hive.openBox('info');
-            box.put('info', false);
-          }
-        }
-      },
-    );
+    //       if (message.data['update'] != null) {
+    //         final box = await Hive.openBox('info');
+    //         box.put('info', false);
+    //       }
+    //     }
+    //   },
+    // );
 
-    // 2. This method only call when App in forground it mean app must be opened
-    FirebaseMessaging.onMessage.listen(
-      (message) async {
-        if (message.notification != null) {
-          Get.dialog(const Text('hello'));
+    // // 2. This method only call when App in forground it mean app must be opened
+    // FirebaseMessaging.onMessage.listen(
+    //   (message) async {
+    //     if (message.notification != null) {
+    //       Get.dialog(const Text('hello'));
 
-          if (message.data["update"]) {
-            final box = await Hive.openBox('info');
-            box.put('info', false);
-            Get.defaultDialog(
-                title: 'Update Available',
-                middleText: 'Synchronized to get latest updates',
-                onCancel: () {},
-                onConfirm: () {},
-                textConfirm: "Synchronized");
-          }
-        }
-      },
-    );
+    //       if (message.data["update"]) {
+    //         final box = await Hive.openBox('info');
+    //         box.put('info', false);
+    //         Get.defaultDialog(
+    //             title: 'Update Available',
+    //             middleText: 'Synchronized to get latest updates',
+    //             onCancel: () {},
+    //             onConfirm: () {},
+    //             textConfirm: "Synchronized");
+    //       }
+    //     }
+    //   },
+    // );
 
-    // 3. This method only call when App in background and not terminated(not closed)
-    FirebaseMessaging.onMessageOpenedApp.listen(
-      (message) async {
-        if (message.notification != null) {
-          Get.dialog(const Text('hello'));
-          final box = await Hive.openBox('info');
-          box.put('info', false);
-        }
-      },
-    );
+    // // 3. This method only call when App in background and not terminated(not closed)
+    // FirebaseMessaging.onMessageOpenedApp.listen(
+    //   (message) async {
+    //     if (message.notification != null) {
+    //       Get.dialog(const Text('hello'));
+    //       final box = await Hive.openBox('info');
+    //       box.put('info', false);
+    //     }
+    //   },
+    // );
 
     FlutterNativeSplash.remove();
   }
