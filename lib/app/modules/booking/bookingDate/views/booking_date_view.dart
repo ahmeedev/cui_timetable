bd30357c73faec1c618e2fe8255e8a59d37f9871
Ahmad Tariq <75003816+ahmadbintariq4u@users.noreply.github.com>
2023-01-14 21:13:10 +0500

    working on date picker

--- lib/app/modules/booking/bookingDate/views/booking_date_view.dart
+++ lib/app/modules/booking/bookingDate/views/booking_date_view.dart
@@ -1,6 +1,9 @@
+import 'package:cui_timetable/app/modules/booking/bookingDetails/controllers/booking_details_controller.dart';
 import 'package:cui_timetable/app/theme/app_colors.dart';
 import 'package:cui_timetable/app/theme/app_constants.dart';
+import 'package:cui_timetable/app/utilities/time/cloud_time.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 import 'package:get/get.dart';
 import 'package:syncfusion_flutter_datepicker/datepicker.dart';
@@ -21,11 +24,36 @@ class BookingDateView extends GetView<BookingDateController> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              Expanded(
-                child: SfDateRangePicker(
-                  backgroundColor: widgetColor,
-                ),
-              ),
+              FutureBuilder<DateTime>(
+                  future: currentTime(),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      return Expanded(
+                        child: SfDateRangePicker(
+                          backgroundColor: widgetColor,
+                          minDate: snapshot.data,
+                          maxDate: snapshot.data!.add(const Duration(days: 14)),
+                          selectableDayPredicate: (date) {
+                            final bookingDay =
+                                Get.find<BookingDetailsController>().bookingDay;
+
+                            if (date.weekday == bookingDay) {
+                              // logger.i('yes');
+                              return true;
+                            } else {
+                              return false;
+                            }
+
+                            // If the user select the date on weekended
+                          },
+                        ),
+                      );
+                    }
+
+                    return const Expanded(
+                      child: SpinKitFadingCube(color: primaryColor, size: 30.0),
+                    );
+                  }),
               ElevatedButton(
                   onPressed: () {
                     Get.back();
